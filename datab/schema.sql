b509cd6e16e704318fa5fae0e00afc4fc1195634
Taimuraz Baskaev <osnovatahmuraz@gmail.com>
2024-12-11 22:32:13 +0300

    merge

--- datab/schema.sql
+++ datab/schema.sql
@@ -1,10 +1,3 @@
--- CREATE TABLE movies (
---     id SERIAL PRIMARY KEY,
---     title VARCHAR(255) NOT NULL,
---     description TEXT,
---     release_date DATE
--- );
-
 CREATE TABLE movies (
     code VARCHAR(255) PRIMARY KEY, 
     title VARCHAR(255) NOT NULL,   
